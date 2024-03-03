aa1f4348fd122027a08177d06b487f5d9208ba3f
Mohamed Slimane <mohamed.slimane@esprit.tn>
2024-03-03 12:26:39 +0100

    minor improvments and cleanup

--- dbconnection.cpp
+++ dbconnection.cpp
@@ -15,18 +15,13 @@ bool dbconnection::createconnect()
     QSettings settings("config.ini", QSettings::IniFormat);
     QString username = settings.value("Database/Username", "DefaultUsername").toString();
     QString password = settings.value("Database/Password", "DefaultPassword").toString();
-    qDebug() << "Username from settings: " << username;
-    qDebug() << "Password from settings: " << password;
     QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
     db.setDatabaseName("TVEmpire");
     db.setUserName(username);
     db.setPassword(password);
-    qDebug() << username << password;
 
     if (db.open())
-    {
         test = true;
-    }
 
     return test;
 }
