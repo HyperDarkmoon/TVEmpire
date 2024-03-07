d8a7d3fd57d72da7ce55767f816a6b46fb876cbf
med yassine <antar.yassino@gmail.com>
2024-03-07 21:41:23 +0100

    test test

--- dbconnection.cpp
+++ dbconnection.cpp
@@ -16,9 +16,10 @@ bool dbconnection::createconnect()
     QString username = settings.value("Database/Username", "DefaultUsername").toString();
     QString password = settings.value("Database/Password", "DefaultPassword").toString();
     QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+    qDebug() << username << "   " << password;
     db.setDatabaseName("TVEmpire");
-    db.setUserName("antar");
-    db.setPassword("admin");
+    db.setUserName(username);
+    db.setPassword(password);
 
     if (db.open())
         test = true;
